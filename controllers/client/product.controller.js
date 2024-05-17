d1da8924b67db70b515ab212f8cd05fb97375ccf
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-05-17 21:17:47 +0700

    layout dashboard admin

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,22 +1,19 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-    try {
         const products = await Product.find({
             status: "active",
             deleted: false
         });
+        // console.log(products);
         const newProducts = products.map(item => {
            item.pricenew = (item.price - item.price * item.discountPercentage / 100).toFixed(0);
            return item
         });
-        console.log(products);
+
         res.render('client/pages/products/index', {
             pagetitle: "List of Products",
             products: newProducts
         });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    }
+    
 };
--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 require("dotenv").config();
-
+const systemConfig = require("./config/system");
 const database = require('./config/database');
 database.connect();
-
 
 const port = process.env.PORT;
 const app = express();
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+//App local variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+
 app.use(express.static("public"));
 //Routes
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}`);
