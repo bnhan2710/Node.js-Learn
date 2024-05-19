fa66044fe07024bcb61a6d1fb2374ee35f754961
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-05-19 12:03:10 +0700

    #4.1 pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,31 +1,49 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
-const searchHelper = require("../../helpers/search");
-//[GET] /admin/products
-module.exports.index = async (req, res) => { 
+const paginationHelper = require("../../helpers/pagination.helper");
+const systemConfig = require("../../config/system");
 
-  //Bộ lọc
-  let filterStatus = filterStatusHelper(req.query)
-  const find = {
-    deleted: false,
-  }
+// [GET] /admin/products/
+module.exports.index = async (req, res) => {
+  try {
+    // Status Filter
+    const filterStatus = filterStatusHelper(req.query);
+    // End Status Filter
 
-  if(req.query.status) {
-    find.status = req.query.status;
-  }
+    const find = {
+      deleted: false
+    }
 
-  //Tìm kiếm
-  const objSearch = searchHelper(req.query);
-  if(objSearch.regex) {
-    find.title = objSearch.regex
-  }
-        const products = await Product.find(find);
-        // console.log(products);
-        res.render('admin/pages/products/index', {
-            pageTitle: "Danh sách sản phẩm",
-            products: products,
-            filterStatus: filterStatus,
-            keyword : objSearch.keyword
-        });
+    if(req.query.status) {
+      find.status = req.query.status;
+    }
+
+    // Search
+    if(req.query.keyword) {
+      const regex = new RegExp(req.query.keyword, "i");
+      find.title = regex;
+    }
+    // End Search
+
+    // Pagination
+    const countProducts = await Product.countDocuments(find);
+    const objectPagination = paginationHelper(6, req.query, countProducts);
+    // End Pagination
+
+    const products = await Product.find(find)
+      .limit(objectPagination.limitItems)
+      .skip(objectPagination.skip);
     
-};
+    res.render("admin/pages/products/index", {
+      pageTitle: "Danh sách sản phẩm",
+      products: products,
+      filterStatus: filterStatus,
+      keyword: req.query.keyword,
+      pagination: objectPagination,
+      startID : objectPagination.skip
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/${systemConfig.prefixAdmin}/products`);
+  }
+}
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -43,3 +43,20 @@ if(buttonsStatus.length > 0) {
     }
 
 //End Form Search
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if(buttonsPagination.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsPagination.forEach(button => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+
+      url.searchParams.set("page", page);
+
+      window.location.href = url.href;
+    });
+  });
+}
+// End Pagination
